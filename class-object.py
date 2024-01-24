3c38f6e8f013666d63c6f9f5ef5a297f4b519a1d
Prem Sai Pillutla <premtechspace@gmail.com>
2024-01-24 17:37:04 +0530

    Class and Objects
    Simple implementation of Class and Objects with an example called Car.

---
+++ class-object.py
@@ -0,0 +1,37 @@
+class Car:
+    def __init__(self, make, model, year, color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+        self.is_running = False
+
+    def start_engine(self):
+        if not self.is_running:
+            print(f"The {self.color} {self.year} {self.make} {self.model}'s engine is now running.")
+            self.is_running = True
+        else:
+            print("The engine is already running.")
+
+    def stop_engine(self):
+        if self.is_running:
+            print(f"The {self.color} {self.year} {self.make} {self.model}'s engine is now stopped.")
+            self.is_running = False
+        else:
+            print("The engine is already stopped.")
+
+    def display_info(self):
+        print(f"{self.year} {self.make} {self.model} ({self.color})")
+        print("Status: Running" if self.is_running else "Status: Stopped")
+
+# Creating instances of the Car class
+car1 = Car("Toyota", "Camry", 2020, "Blue")
+car2 = Car("Honda", "Accord", 2021, "Red")
+
+# Accessing attributes and calling methods
+car1.start_engine()
+car2.start_engine()
+car1.display_info()
+car2.display_info()
+car1.stop_engine()
+car2.stop_engine()
