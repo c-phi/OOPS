4a92cac788a48e34537fe8dfd850acb94d4dd628
Prem Sai Pillutla <premtechspace@gmail.com>
2024-01-25 09:53:42 +0530

    Creating two private attributes
    Required __ before the variable

--- encaspsulate.py
+++ encaspsulate.py
@@ -1,31 +1,32 @@
 # Implementing encapsulation by creating two protected attributes.
+# Implementing encapsulation by creating two private attributes.
 class Car:
     def __init__(self, make, model, year, color):
         # Encapsulation requires an underscore.
         self._make = make   # 4 attributes
         self._model = model
-        self.year = year
-        self.color = color
+        self.__year = year
+        self.__color = color
         self.is_running = False
 
         # 3 methods 
 
     def start_engine(self):
         if not self.is_running:
-            print(f"The {self.color} {self.year} {self._make} {self._model}'s engine is now running.")
+            print(f"The {self.__color} {self.__year} {self._make} {self._model}'s engine is now running.")
             self.is_running = True
         else:
             print("The engine is already running.")
 
     def stop_engine(self):
         if self.is_running:
-            print(f"The {self.color} {self.year} {self._make} {self._model}'s engine is now stopped.")
+            print(f"The {self.__color} {self.__year} {self._make} {self._model}'s engine is now stopped.")
             self.is_running = False
         else:
             print("The engine is already stopped.")
 
     def display_info(self):
-        print(f"{self.year} {self._make} {self._model} ({self.color})")
+        print(f"{self.__year} {self._make} {self._model} ({self.__color})")
         print("Status: Running" if self.is_running else "Status: Stopped")
 
     
